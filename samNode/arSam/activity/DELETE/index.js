02431de077978e6bb3467706294cf0d9581db2cd
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2024-05-16 22:03:18 +0000

    import logger for activity delete

--- samNode/arSam/activity/DELETE/index.js
+++ samNode/arSam/activity/DELETE/index.js
@@ -1,3 +1,5 @@
+const { logger } = require("/opt/loggerLayer");
+
 exports.handler = async (event, context) => {
     logger.debug("Activity delete:", event);
     return sendResponse(501, { msg: "Error: Not implemented." }, context);
